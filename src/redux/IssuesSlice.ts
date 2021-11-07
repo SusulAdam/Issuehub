bd7953f5780137e4c15a36feb7733ee17cdf4201
Adam Susuł <adamsusul@Adams-MacBook-Pro.local>
2021-11-07 09:19:41 +0100

    add fetch next page

--- src/redux/IssuesSlice.ts
+++ src/redux/IssuesSlice.ts
@@ -3,18 +3,22 @@ import {fetchIssues} from './api';
 
 export type Issue = {
   title: string;
+  url: string;
+  id: number;
 };
 
 export type IssuesSliceState = {
   issues: Issue[];
   issuesError: boolean;
   issuesLoading: boolean;
+  nextPage: number;
 };
 
 const initialState: IssuesSliceState = {
   issues: [],
   issuesLoading: false,
   issuesError: false,
+  nextPage: 1,
 };
 
 export const fetchIssuesPagination = createAsyncThunk<
@@ -39,12 +43,14 @@ const issuesSlice = createSlice({
   extraReducers: builder => {
     builder
       .addCase(fetchIssuesPagination.fulfilled, (state, action) => {
-        state.issues = action.payload.issues;
+        state.issues = state.issues.concat(action.payload.issues);
         state.issuesError = false;
         state.issuesLoading = false;
+        state.nextPage += 1;
       })
       .addCase(fetchIssuesPagination.pending, state => {
         state.issuesLoading = true;
+        state.issuesError = false;
       })
       .addCase(fetchIssuesPagination.rejected, state => {
         state.issuesError = true;
--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 import {homeStyles} from './HomeScreen.style';
 import {useDispatch, useSelector} from 'react-redux';
 import {RootState} from '../../redux/store';
@@ -14,18 +14,35 @@ const HomeScreen = (): JSX.Element => {
     dispatch(fetchIssuesPagination({page: 1}));
   }, [dispatch]);
 
+  const handleOnEndEached = () => {
+    if (!reduxState.issuesError && !reduxState.issuesLoading) {
+      dispatch(fetchIssuesPagination({page: reduxState.nextPage}));
+    }
+  };
+
   return (
     <>
-      {!reduxState.issuesError && reduxState.issuesLoading && <Loading />}
+      {reduxState.issuesLoading && <Loading />}
       {reduxState.issuesError && (
         <Error>
           <Text>Error- please reload app</Text>
         </Error>
       )}
       {!reduxState.issuesLoading && !reduxState.issuesError && (
-        <View style={homeStyles.container}>
-          <Text>{JSON.stringify(reduxState.issues[0])}</Text>
-        </View>
+        <FlatList
+          data={reduxState.issues}
+          keyExtractor={(_, index) => `key: ${index}`}
+          onEndReached={handleOnEndEached}
+          renderItem={item => {
+            return (
+              <View style={homeStyles.container}>
+                <Text>{item.item.id}</Text>
+                <Text>{item.item.title}</Text>
+                <Text>{item.item.url}</Text>
+              </View>
+            );
+          }}
+        />
       )}
     </>
   );
